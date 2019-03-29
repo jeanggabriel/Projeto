6f7e1cca144fe8d55d389b35137a14df13b02e17
jean_ggabriel <jeangabriel310@gmail.com>
2019-03-28 23:51:18 -0300

    funçao para adicionar dados no banvo

--- node-str/node_js_API/index.js
+++ node-str/node_js_API/index.js
@@ -1,60 +1,52 @@
 'use trict'
-///
-///const express = require("express");
-///const bodyParser = require("body-parser");
-//const app = express( );
-//app.use(bodyParser.json());
-//app.use(bodyParser.urlencoded({extended:false}));
-//app.get('/', (req,res) => {
-// res.send("ok API REST FRETE-FACIL ");
-//});
-//app.listen(3000);
-///	
 const express = require('express');
 const app = express();         
 const bodyParser = require('body-parser');
 const port = 3000; //porta padrão
 const mysql = require('mysql');
-
+//configurando o body parser para pegar POSTS mais tarde
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
-
-const mysqlConnection = mysql.createConnection({
-    host:'localhost',
-    user: 'root',
-    password: '',
-    database: 'projeto'
-});
-
-mysqlConnection.connect((err)=>{
-    if(!err)
-    console.log("db connection sucess");
-    else
-    console.log("connection failed \nerr");
-});
-app.listen(3000,()=>console.log("express server "));
-app.get("projeto",(res,req)=>{
-    mysqlConnection.query("SELECT * FROM login",(err, rows, fields)=>{
-        if (!err)
-        res.send(rows);
-        else
-        console.log(err);
+//definindo as rotas
+const router = express.Router();
+router.get('/', (req, res) => res.json({ message: 'Funcionando!' }));
+app.use('/', router);
+app.listen(port);
+console.log('API funcionando!');
+  function execSQLQuery(sqlQry, res){
+    const connection = mysql.createConnection({
+      host     : 'localhost',
+      port     : 3306,
+      user     : 'root',
+      password : '',
+      database : 'projeto'
     });
-});
-app.listen(3000,()=>console.log("express server "));
-app.get("projeto",(res,req)=>{
-    mysqlConnection.query("SELECT * FROM carga",(err, rows, fields)=>{
-        if (!err)
-        res.send(rows);
+    connection.query(sqlQry, function(error, results, fields){
+        if(error) 
+          res.json(error);
         else
-        console.log(err);
+          res.json(results);
+        connection.end();
+        console.log('executou!');
     });
-});
-app.listen(3000,()=>console.log("express server "));
-app.get("projeto",(res,req)=>{
-    mysqlConnection.query("SELECT * FROM caminhao",(err, rows, fields)=>{
-        if (!err)
-        res.send(rows);
-        else
-        console.log(err);
-    });
+  }
+router.get('/login', (req, res, get) =>{
+      execSQLQuery('SELECT * FROM login', res);
+  });    
+router.get('/login/:id?', (req, res) =>{
+    let filter = '';
+    if(req.params.id) filter = ' WHERE ID=' + parseInt(req.params.id);
+    execSQLQuery('SELECT * FROM login' + filter, res);
+})
+router.delete('/login/:id', (req, res) =>{
+    execSQLQuery('DELETE FROM login WHERE ID=' + parseInt(req.params.id), res);
+})
+router.post('/login', (req, res) =>{
+    const nome = req.body.nome.substring(0,45);
+    const telefone = req.body.telefone.substring(0,45);
+    const email = req.body.email.substring(0,45);
+    const senha = req.body.senha.substring(0,45);
+    const tipo_de_cadastro = req.body.tipo_de_cadastro.substring(0,45);
+    const foto = req.body.foto.substring(0,255)
+    execSQLQuery(`INSERT INTO login(nome, telefone, email, senha, tipo_de_cadastro, foto) VALUES('${nome}','${telefone}','${email}','${senha}','${tipo_de_cadastro}','${foto})`, res);
 });
