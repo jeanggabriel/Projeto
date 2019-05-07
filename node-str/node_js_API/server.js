10646a0221a2e158082464bf84de8fb08920f262
jean_ggabriel <jeangabriel310@gmail.com>
2019-05-07 13:56:56 -0300

    json

--- node-str/node_js_API/server.js
+++ node-str/node_js_API/server.js
@@ -1,16 +1,13 @@
 var axios = require("axios");
-axios.get("http://localhost:3000/").then(function(resposta){
+axios.get("http://localhost:3000/user").then(function(resposta){
     console.log(resposta.data);
 });
-axios.get("http://localhost:3000/user").then(function(data){
-    console.log(data);
+axios.get("http://localhost:3000/user_login").then(function(resposta){
+    console.log(resposta.data);
 });
-axios.get("http://localhost:3000/user_login").then(function(data){
-    console.log(data);
+axios.get("http://localhost:3000/user_carga").then(function(resposta){
+    console.log(resposta.data);
 });
-axios.get("http://localhost:3000/user_carga").then(function(data){
-    console.log(data);
-});
-axios.get("http://localhost:3000/user_caminhao").then(function(data){
-    console.log(data);
+axios.get("http://localhost:3000/user_caminhao").then(function(resposta){
+    console.log(resposta.data);
 });
