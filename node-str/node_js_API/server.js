a38e1da935dd3e9137b339f0fdae13fe68e73f08
jean_ggabriel <jeangabriel310@gmail.com>
2019-05-16 21:40:30 -0300

    some text

--- node-str/node_js_API/server.js
+++ node-str/node_js_API/server.js
@@ -1,4 +1,13 @@
-const PORT = process.env.PORT || config.httpPort;
-const PORT = process.env.PORT||'8080';
+const http = require("http");
+// const port = process.env.PORT || 8080
+http.listen((process.env.PORT || 5000), function(){
+  console.log('listening on *:5000');
+});
+// const app = require("./app");
 
-app.listen(PORT, ...)
+// const server = http.createServer(app);
+// app.use('./app',app);
+// server.listen(port, () => {
+// //     console.log('API Ligada e rodando na porta : ' + port);
+// // });
+module.exports = server.js
